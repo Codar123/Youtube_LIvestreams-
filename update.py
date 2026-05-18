f1306578c1a535f5e74ae3068bbe947e9fedb666
Codar123 <greencx121@gmail.com>
2026-05-18 07:45:02 +0545

    Update and rename animal_planet_proxy.py to update.py

---
+++ update.py
@@ -0,0 +1,16 @@
+import subprocess
+
+YOUTUBE_URL = "https://youtube.com/watch?v=RYhbprsJWUQ"
+
+url = subprocess.check_output(
+    ["yt-dlp", "-g", YOUTUBE_URL],
+    text=True
+).strip()
+
+m3u = f"""#EXTM3U
+#EXTINF:-1,Live Channel
+{url}
+"""
+
+with open("live.m3u", "w") as f:
+    f.write(m3u)
